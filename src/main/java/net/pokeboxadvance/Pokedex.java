e59a2a1ba45ce0d23ab22cc988adedf04c5a6dc4
AL4CR1TY <al4cr1ty@pokeboxadvance.net>
2018-05-29 03:27:18 +0200

    Add pokedex

---
+++ src/main/java/net/pokeboxadvance/Pokedex.java
@@ -0,0 +1,70 @@
+package net.pokeboxadvance;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+/**
+ * Pokédex.
+ *
+ * This class represents a Pokédex and is more or less a list of {@link DexPokemon}. It also
+ * contains several methods for managing the dex.
+ *
+ * @author AL4CR1TY
+ * @version %I%
+ * @since 2018-05-29
+ */
+public class Pokedex {
+
+  private static final Logger LOGGER = LogManager.getLogger();
+
+  private ArrayList<DexPokemon> pokedex = new ArrayList<>();
+  private Comparator<DexPokemon>
+      numberComparator = (DexPokemon pokemon1, DexPokemon pokemon2)
+          -> Integer.compare(pokemon1.getNumber(), pokemon2.getNumber()),
+      nameComparator = (DexPokemon pokemon1, DexPokemon pokemon2)
+          -> pokemon1.getName().toLowerCase().compareTo(pokemon2.getName().toLowerCase());
+
+  /**
+   * Adds a Pokemon to the Pokedex.
+   *
+   * @param pokemon the Pokemon to be added
+   */
+  public void add(DexPokemon pokemon) {
+    this.pokedex.add(pokemon);
+  }
+
+  /**
+   * Sorts the Pokedex by Pokemon number.
+   */
+  public void sortByNumber() {
+    this.pokedex.sort(numberComparator);
+  }
+
+  /**
+   * Sorts the Pokedex by Pokemon number.
+   */
+  public void sortByName() {
+    this.pokedex.sort(nameComparator);
+  }
+
+  /**
+   * Returns a Pokemon with a number matching the provided one. If no Pokemon with that number exists, the first one is returned. If the Pokedex is otherwise empty, null is returned.
+   *
+   * @param number a Pokemon's number
+   * @return the Pokemon with a number matching the one provided
+   */
+  public DexPokemon getByNumber(int number) {
+    if(this.pokedex.size() == 0) {
+      LOGGER.warn("Pokedex is empty.");
+      return null;
+    }
+    for (DexPokemon pokemon : this.pokedex) {
+      if(pokemon.getNumber() == number) {
+        return pokemon;
+      }
+    }
+    return this.pokedex.get(0);
+  }
+}
